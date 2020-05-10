1d3010c63dd4d24a772e5616ec4e4c865dab6e5a
guardianagnel <58181388+guardianagnel@users.noreply.github.com>
2020-05-10 13:54:10 +0300

    Update app.cs

--- app.cs
+++ app.cs
@@ -1,1 +1,4 @@
 var connectionString = "mongodb+srv://gurdian-db:eYRGu3MOgdcb3F26@gurdian-com-g4rrm.mongodb.net/gurdian?retryWrites=true&w=majority";
+
+
+"mongodb+srv://gurdian-db:{val}@gurdian-com-g4rrm.mongodb.net/gurdian?retryWrites=true&w=majority"
